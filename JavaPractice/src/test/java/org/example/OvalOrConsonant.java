80292df9564d0cc06632bbe0462906f639fef5a7
VishwanathChenni <vishwanathchenni@gmail.com>
2022-11-20 14:06:22 +0800

    Renamed file

--- JavaPractice/src/test/java/org/example/OvalOrConsonant.java
+++ JavaPractice/src/test/java/org/example/OvalOrConsonant.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Scanner;
 
-public class OvelOrConsonant {
+public class OvalOrConsonant {
   public static void main(String args[]){
       Scanner scanner=new Scanner(System.in);
       System.out.println("Enter a Char");
       char ch=scanner.next().charAt(0);
       if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'  ){
-          System.out.println("Its a Ovel");
+          System.out.println("Its a Oval");
       }else{
           System.out.println("Its a Consonant");
       }
