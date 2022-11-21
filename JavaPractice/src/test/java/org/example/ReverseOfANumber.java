95c2c72bdc83de8efdddef1f6a6d008d868bfaf6
VishwanathChenni <vishwanathchenni@gmail.com>
2022-11-22 06:42:34 +0800

    Reverse a number

---
+++ JavaPractice/src/test/java/org/example/ReverseOfANumber.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class ReverseOfANumber {
+    @Test
+    public void fnReverseOfANumber(){
+        int iMyNumber=1234;
+        int rev=0;
+        while(iMyNumber !=0){
+            int Number=iMyNumber % 10;
+            rev=rev *10 +Number;
+            iMyNumber=iMyNumber/10;
+        }
+        System.out.println("Rveresed Number:-"+rev);
+    }
+}
