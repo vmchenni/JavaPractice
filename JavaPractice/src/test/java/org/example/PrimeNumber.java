afa83a2d2819397ff2ea40a2aedcb78d49db9940
VishwanathChenni <vishwanathchenni@gmail.com>
2022-11-23 07:10:10 +0800

    PrimeNumber

---
+++ JavaPractice/src/test/java/org/example/PrimeNumber.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimeNumber {
+    @Test
+    public void fnPrimeNumber(){
+        int iNumber=13;
+        boolean iReponse=false;
+        for(int i=2;i<iNumber/2;i++){
+                if((iNumber % i)==0){
+                    iReponse=true;
+                    break;
+                }
+        }
+        if(iReponse){
+            System.out.println("The number "+iNumber+" is not a prime number");
+        }else{
+            System.out.println("The number "+iNumber+" is a prime number");
+        }
+
+    }
+}
