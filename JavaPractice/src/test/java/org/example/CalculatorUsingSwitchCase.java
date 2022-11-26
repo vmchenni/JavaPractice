ef7d62a500ccc63490527de783b9582c4e0cb86f
VishwanathChenni <vishwanathchenni@gmail.com>
2022-11-26 16:14:18 +0800

    Calculator

--- JavaPractice/src/test/java/org/example/CalculatorUsingSwitchCase.java
+++ JavaPractice/src/test/java/org/example/CalculatorUsingSwitchCase.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Scanner;
 
 public class CalculatorUsingSwitchCase {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Enter first number");
         int iFirstNumber= new Scanner(System.in).nextInt();
 
@@ -11,18 +11,13 @@ public class CalculatorUsingSwitchCase {
         char chOperator=new Scanner(System.in).next().charAt(0);
 
         System.out.println("Enter second number");
-        int iSeondNumber=new Scanner(System.in).nextInt();
+        int iSecondNumber=new Scanner(System.in).nextInt();
 
-        double dResult=0;
-        switch (chOperator){
-            case '+':
-                System.out.println(iFirstNumber+iSeondNumber);break;
-            case '-':
-                System.out.println(iFirstNumber-iSeondNumber);break;
-            case '*':
-                System.out.println(iFirstNumber*iSeondNumber);break;
-            case '%':
-                System.out.println(iFirstNumber/iSeondNumber);break;
+        switch (chOperator) {
+            case '+' -> System.out.println(iFirstNumber + iSecondNumber);
+            case '-' -> System.out.println(iFirstNumber - iSecondNumber);
+            case '*' -> System.out.println(iFirstNumber * iSecondNumber);
+            case '%' -> System.out.println(iFirstNumber / iSecondNumber);
         }
     }
 }
