57702e1eb5e4057cb788bbf14f59de757487c62b
VishwanathChenni <vishwanathchenni@gmail.com>
2022-11-27 12:44:40 +0800

    fnFindSmallestAndLargestNumberInArray

--- JavaPractice/src/test/java/org/example/FindSmallestAndLargestNumberInArray.java
+++ JavaPractice/src/test/java/org/example/FindSmallestAndLargestNumberInArray.java
@@ -1,23 +1,21 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
 public class FindSmallestAndLargestNumberInArray {
     @Test
     public void fnFindSmallestAndLargestNumberInArray(){
-        int[] arr={10,24,50,-125,987};
+        int[] arr={10,-20,-30,50,999};
         int iSmall=arr[0];
         int iBig=arr[0];
         for(int i=1;i<arr.length;i++){
-            if(arr[i]>iBig){
-                iBig=arr[i];
-            }else if(arr[i]<iSmall){
+            if(arr[i]< iSmall){
                 iSmall=arr[i];
+            } else if (arr[i]> iBig) {
+                iBig=arr[i];
             }
         }
-        System.out.println(Arrays.toString(arr));
-        System.out.println("Largest number is :- "+iBig+" and Smallest number is :- "+iSmall+"");
+        System.out.println("Array is :-"+Arrays.toString(arr));
+        System.out.println("The small number is "+iSmall+" and Bing number is "+iBig+"");
     }
 }
