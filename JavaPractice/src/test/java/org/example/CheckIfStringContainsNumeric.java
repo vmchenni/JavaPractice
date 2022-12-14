fcf83d447e038344d896fa8e74e3d5ccb46d9649
VishwanathChenni <vishwanathchenni@gmail.com>
2022-12-14 12:02:38 +0800

    CheckIfStringContainsNumeric

---
+++ JavaPractice/src/test/java/org/example/CheckIfStringContainsNumeric.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckIfStringContainsNumeric {
+    @Test
+    public void fnCheckIfStringContainsNumeric(){
+        String str="12HelloWorld";
+        System.out.println(isContainsNumeric(str));
+
+    }
+
+    private boolean isContainsNumeric(String str) {
+        boolean isContainNumber=false;
+        for(int i=0;i< str.length();i++){
+            if(Character.isDigit(str.charAt(i))){
+               isContainNumber=true;
+               break;
+            }
+        }
+        return isContainNumber;
+    }
+}
