f06111e4e6799f48c7d65a2f37deb0b86308b7f9
VishwanathChenni <vishwanathchenni@gmail.com>
2022-12-22 12:19:33 +0800

    Print One to Hndred

--- JavaPractice/src/test/java/org/example/PrintOneToHundredWithoutUsingLoop.java
+++ JavaPractice/src/test/java/org/example/PrintOneToHundredWithoutUsingLoop.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 
 public class PrintOneToHundredWithoutUsingLoop {
     @Test
-    public void callingFunction(){
-        sPrint(10);
+    public void fnPrintOneToHundred(){
+        fnPrint(1);
     }
-    public void sPrint(int  i){
-        if(i >=0 ){
-            System.out.print(" "+i);
-            sPrint(i-1);
+
+    private void fnPrint(int i) {
+        if(i <= 100){
+            System.out.println(i);
+            fnPrint(i+1);
         }
 
     }
